6c085cab6c2833fe859950cdb019ef5e06850699
TheBoringDude <iamcoderx@gmail.com>
2022-11-18 00:37:29 +0800

    change atomicassets api endpoint

--- lib/atomicassets.go
+++ lib/atomicassets.go
@@ -1,5 +1,9 @@
 package lib
 
-import "github.com/World-of-Cryptopups/atomicassets-go"
+import (
+	"net/http"
 
-var Atom = atomicassets.New()
+	"github.com/World-of-Cryptopups/atomicassets-go"
+)
+
+var Atom, _ = atomicassets.NewCustom("https://atomic.wax.eosrio.io/atomicassets/v1", &http.Client{})
